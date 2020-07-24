ca7a92874c0c3099044f5dd38bfc548bfc3f9900
cunweizhao <18500103508@163.com>
2020-07-24 18:48:53 +0800

    useEffect 不需要清除的 Effect_

--- react-hooks/src/components/LikeButton.js
+++ react-hooks/src/components/LikeButton.js
@@ -1,8 +1,12 @@
-import React,{useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const LikeButton = () =>{
     const [like, setLike] = useState(0)
     const [on,setOn] = useState(true)
+    //dom更新之后调用
+    useEffect( () =>{
+        document.title = `点击了${like}次`
+    })
     return (
         <>
         <button onClick ={ () =>{setLike(like +1 )}}>
