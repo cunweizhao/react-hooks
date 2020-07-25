0103aa64be1611f7f1798221b601d602e629a712
cunweizhao <18500103508@163.com>
2020-07-25 09:11:31 +0800

    useEffect 需要清除的 Effect

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LikeButton from './components/LikeButton'
-
+import MoustTracker from "./components/MouseTracker";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MoustTracker/>
        <LikeButton/>
         <a
           className="App-link"
